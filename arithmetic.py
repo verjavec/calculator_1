e082f6136e1eabb8a1f54b7dd3824ddb062ab913
Vicki Erjavec <verjavec@gmail.com>
2021-01-07 00:10:47 +0000

    Add the functions

---
+++ arithmetic.py
@@ -0,0 +1,58 @@
+"""Functions for common math operations."""
+
+
+def add(num1, num2):
+    """Return the sum of the two inputs."""
+
+    # Add num1 plus num2
+    # Return the result
+    print(num1 + num2)
+    return(num1 + num2)
+
+
+def subtract(num1, num2):
+    """Return the second number subtracted from the first."""
+
+    # Subtract num2 from num1
+    # Return the result
+    return(num1 - num2)
+
+def multiply(num1, num2):
+    """Multiply the two inputs together."""
+    # Multiply num1 and num2
+    # Return the result
+    return(num1 * num2)
+
+def divide(num1, num2):
+    """Divide the first input by the second and return the result."""
+
+    # Divide num1 by num2
+    # Return the result
+    return(num1 / num2)
+
+def square(num1):
+    """Return the square of the input."""
+    # Find the square of the input
+    # Return the result
+    return(num1**0.5)
+
+def cube(num1):
+    """Return the cube of the input."""
+
+    # Find num1 raised to 3
+    # Return the result
+    return(num1 ** 3)
+
+def power(num1, num2):
+    """Raise num1 to the power of num2 and return the value."""
+
+    # Find num1 raised to the power of num2
+    # Return the result
+    return(num1 ** num2)
+
+def mod(num1, num2):
+    """Return the remainder of num1 / num2."""
+
+    # Find the remainder using num1 mod num2
+    # Return the result
+    return(num1 % num2)
